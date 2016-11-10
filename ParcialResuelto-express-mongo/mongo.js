eeb2c3e3a4289a69267439a0fced016aadc7c3c1
unknown <alumno@LAB02PC15.labs.extension>
2016-11-09 21:17:29 -0300

    import de base y creacion de indices

--- ParcialResuelto-express-mongo/mongo.js
+++ ParcialResuelto-express-mongo/mongo.js
@@ -2,7 +2,7 @@ var MongoClient = require( 'mongodb' ).MongoClient;
 var express = require('express');
 var app = express();
 var rutas = require('./rutas');
-/*
+
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var corsOptions = {
@@ -13,41 +13,41 @@ var corsOptions = {
 app.user(bodyParser.urlencoded({extend:false}));
 app.use(bodyParser.json());
 app.use(cors());
-*/
+
 
 MongoClient.connect('mongodb://localhost:27017/franco', function(err, db) {
   if (err)
    throw err;
 
-     //db.collection('personas')
-      //.find({balance:{$eq:"$2,826.16"},{_id:0,name:1,balance:1})
-      //.sort({balance:1})
-      //.toArray((err, data) => { 
-        /*app.use((req,res,next) => {
-          req.db = db.collection('franco');
-          next();
-        });*/
+     db.collection('personas')
+      .find({gender:"female"})
+      .toArray((err,data) => {
+        if (err){
+          console.log(err);
+          console.log (data);
+        }
+      })
     
 
 
 });
 
-    app.get('/users',(req,res,next) =>{
-          // console.log("users");
-          req.res();/*          
+   /* app.get('/users',(req,res,next) =>{
+           console.log("users");
+          req.res();         
           .db
           .find()
           .toArray(err,data) =>{
             res.json(data);
           });
-        */
+        
 
         });
-        //app.use('/',rutas);
-      //if (err)
-        //console.log(err);
-
-      //console.log(data);
+        app.use('/',rutas);
+      if (err)
+        console.log(err);
+*/
+      console.log(data);
       app.listen(3333,function(){
       console.log('Servidor iniciando ...');
     });
--- ParcialResuelto-express-mongo/rutas/index.js
+++ ParcialResuelto-express-mongo/rutas/index.js
@@ -4,15 +4,15 @@ var router = express.Router();
 
 
 router.get('/users',(req,res,next) =>{
-	// console.log("users");
+	console.log("users");
 	req.json();
-	/*
+	
 	.db
 	.find()
 	.toArray(err,data) =>{
 		res.json(data);
 	});
-*/
+
 
 });
 
